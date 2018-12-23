143a472257ba1020e5c7aef61d500d913d0b9080
esraazaitoun1991 <eng.esraaz1991@gmail.com>
2018-12-23 10:22:18 +0200

    (feat) add navbar for the admin profile

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from './component/header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));
 serviceWorker.unregister();
