879b0e1246315fd204ceba71a7293dbf93728a25
RBK18 <rbk18@RBK18s-Mac-mini.local>
2018-12-24 10:21:54 +0200

    nothing till now

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/App.css';
 import App from './App';
-import User from './Components/User';
+ import User from './Components/User';
 import * as serviceWorker from './serviceWorker';
 
-import Profile from './Components/Profile';
+ import Profile from './Components/Profile';
 
 
 ReactDOM.render(<Profile/>, document.getElementById('root'));
